c09cefa7cfbed411951b0db2d77bc3bc4af7e738
Alejandro Roa <alejodev04@gmail.com>
2020-05-22 18:44:53 -0400

    clients data with auth

--- src/actions/clientAction.js
+++ src/actions/clientAction.js
@@ -1,38 +1,44 @@
 import axios from 'axios'
 import { GET_CLIENTS, ADD_CLIENT, DELETE_CLIENT, LOADING_CLIENTS } from './types'
+import { returnErrors } from './errorAction'
+import { configToken } from './authAction'
 
-export const getClients = () => dispatch => {
+
+export const getClients = () => (dispatch, getState) => {
     dispatch(loadingClients())
     axios
-        .get('http://localhost:4000/api/clients')
+        .get('http://localhost:4000/api/clients', configToken(getState))
         .then(res => {
             dispatch({
                 type: GET_CLIENTS,
                 payload: res.data
             })
         })
+        .catch(err => returnErrors(err.response.data, err.response.status))
 }
 
-export const addClient = client => dispatch => {
+export const addClient = client => (dispatch, getState) => {
     axios
-        .post('http://localhost:4000/api/clients', client)
+        .post('http://localhost:4000/api/clients', client, configToken(getState))
         .then(res => {
             dispatch({
                 type: ADD_CLIENT,
                 payload: res.data.client
             })
         })
+        .catch(err => returnErrors(err.response.data, err.response.status))
 }
 
-export const deleteClient = id => dispatch => {
+export const deleteClient = id => (dispatch, getState) => {
     axios
-        .delete(`http://localhost:4000/api/clients/${id}`)
+        .delete(`http://localhost:4000/api/clients/${id}`, configToken(getState))
         .then(res => {
             dispatch({
                 type: DELETE_CLIENT,
                 payload: id
             })
         })
+        .catch(err => returnErrors(err.response.data, err.response.status))
 }
 
 export const loadingClients = () => {
